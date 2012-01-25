419fbc2b765cc22b63b0505f2cfb947fc0b2a8c1
Frederic -lefred- Descamps <frederic.descamps@percona.com>
2012-01-25 14:00:48 +0100

    fix replication privileges problem
    I may fix the mysql puppet lib later
    
    Signed-off-by: Frederic -lefred- Descamps <frederic.descamps@percona.com>

--- modules/percona/manifests/replication.pp
+++ modules/percona/manifests/replication.pp
@@ -1,11 +1,16 @@
 class percona::replication {
 
+	#mysql_grant { "repl@%":
+      	#	privileges => [ 'repl_slave_priv', 'repl_client_priv' ],
+      	#	#require => File["/root/.my.cnf"],
+    	#}
+
 	 mysql::rights{"Set rights for replication":
                 ensure   => present,
                 database => "*",
                 user     => "repl",
 		host     => "%",
                 password => "repl",
-		priv    => "Repl_slave_priv",
+		priv    => [ 'repl_slave_priv', 'repl_client_priv' ],   # not used here but needed to avoid full privileges
         }
 }
