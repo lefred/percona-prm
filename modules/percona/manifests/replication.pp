c158728150825ec4ee8727ea80f385df7ec9533c
Frederic -lefred- Descamps <frederic.descamps@percona.com>
2012-01-26 18:41:28 +0100

    fix uer creation
    Signed-off-by: Frederic -lefred- Descamps <frederic.descamps@percona.com>

--- modules/percona/manifests/replication.pp
+++ modules/percona/manifests/replication.pp
@@ -22,33 +22,22 @@ class percona::replication {
 
 	exec {
 		"addmysqluser1":	
-			command 	=> "/usr/bin/mysql -e \"CREATE USER 'repl'@'%' IDENTIFIED BY 'repl'\"",
+			command 	=> "/usr/bin/mysql -e \"GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%'; FLUSH PRIVILEGES\"",
 			require		=> Exec['startmysql']
+	
 	}
 
 	exec {
 		"addmysqluser2":	
-			command 	=> "/usr/bin/mysql -e \"GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%'; FLUSH PRIVILEGES\"",
-			require		=> Exec['addmysqluser1']
+			command 	=> "/usr/bin/mysql -e \"GRANT REPLICATION SLAVE ON *.* TO 'repl'@'localhost'\"",
+			require		=> Exec['startmysql']
 	
 	}
+
 	exec {
 		"addmysqluser3":	
-			command 	=> "/usr/bin/mysql -e \"CREATE USER 'repl'@'localhost' IDENTIFIED BY 'repl'\"",
-			require		=> Exec['startmysql']
-	}
-
-	exec {
-		"addmysqluser4":	
-			command 	=> "/usr/bin/mysql -e \"GRANT REPLICATION SLAVE ON *.* TO 'repl'@'localhost'\"",
-			require		=> Exec['addmysqluser3']
-	
-	}
-
-	exec {
-		"addmysqluser5":	
 			command 	=> "/usr/bin/mysql -e \"GRANT ALL PRIVILEGES  ON *.* TO 'repl'@'localhost'; FLUSH PRIVILEGES\"",
-			require		=> Exec['addmysqluser4']
+			require		=> Exec['addmysqluser2']
 	
 
 	}
@@ -62,6 +51,6 @@ class percona::replication {
 	exec {
 		"stopmysql":
                         command         => "/etc/init.d/mysql stop",
-			require => [ Exec['addmysqluser2'], Exec['addmysqluser5'] ],
+			require => [ Exec['addmysqluser1'], Exec['addmysqluser3'] ],
 	}
 }
